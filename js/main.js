7e3d2456c4c2cdb791d8d5af7a113e553105791f
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2023-07-09 12:55:06 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(".program-block").click(function(){
+        $(".program-block").removeClass("active")
+        $(this).addClass("active")
+    })
+
+    // membership
+
+    $(".membership-plan").click(function(){
+        $(".membership-plan").removeClass("active")
+        $(this).addClass("active")
+    })
+
+    // slider-review
+    $(".slider-review").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        nextArrow: ' <button class="btn slick-btn slick-next active"></button>',
+        prevArrow: ' <button class="btn slick-btn slick-prev"></button>',
+    })
+
+    $(".slider-review .slick-btn").click(function (e) { 
+        e.preventDefault();
+        $(".slider-review .slick-btn").removeClass("active")
+        $(this).addClass("active")
+    });
+
+    // burger menu
+    $(".burger").click(function(){
+        $(".burger").toggleClass("active");
+        $(".nav").toggleClass("active");
+    })
+
+    // acount link
+    $(".acuont .link").click(function(e){
+        e.preventDefault()
+        $(".acuont .link").removeClass("active")
+        $(this).addClass("active")
+    })
+
+    // tabs
+    $(".tab-block-title").click(function(){
+        $(".tab-block-title").removeClass("active")
+        $(".tab-content").slideUp();
+        $(this).siblings().slideDown();
+        $(this).addClass("active");
+    })
+});
